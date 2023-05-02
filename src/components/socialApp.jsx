f81c832b4d99b073a72c63fa0f06d50aed6ed8af
Anil chauhan <66659663+aniilchauhan@users.noreply.github.com>
2023-05-02 03:35:28 +0000

    style changes

--- src/components/socialApp.jsx
+++ src/components/socialApp.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Tooltip from "@mui/material/Tooltip";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
-const socialApp = () => {
+const SocialIcon = styled(Avatar)(({ theme }) => ({
+  width: 32,
+  height: 32,
+  margin: theme.spacing(0.5),
+}));
+
+const SocialIcons = ({ accounts }) => {
   return (
-    <div>socialApp</div>
-  )
-}
+    <Box>
+      {accounts.map((account) => (
+        <Tooltip title={account.name}>
+          <SocialIcon>
+            {account.type === "facebook" && <FacebookIcon />}
+            {account.type === "twitter" && <TwitterIcon />}
+            {account.type === "instagram" && <InstagramIcon />}
+          </SocialIcon>
+        </Tooltip>
+      ))}
+    </Box>
+  );
+};
 
-export default socialApp
+export default SocialIcons;
--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
-import { Box, Container, Grid, TextField, Button, Typography } from '@mui/material';
+import React, { useState } from "react";
+import {
+  Box,
+  Container,
+  Grid,
+  TextField,
+  Button,
+  Typography,
+} from "@mui/material";
 
 const ContactMe = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
   const [submitting, setSubmitting] = useState(false);
   const [submitted, setSubmitted] = useState(false);
@@ -18,15 +25,65 @@ const ContactMe = () => {
   };
 
   return (
-    <Box sx={{ backgroundColor: 'blue', py: 2 ,width:"100%"}}>
-        <Grid container spacing={3} sx={{backgroundColor: 'pink',width:"100%",margin:"auto"}}>
-          <Grid item xs={12} md={6} lg={6}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%' }}>
-              <Typography variant="h6" gutterBottom>Contact Information</Typography>
-              <Typography variant="body1" sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
-                <span style={{ marginRight: '8px' }}>Name:</span> John Doe
-              </Typography>
-              <Typography variant="body1" sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+    <Box sx={{ backgroundColor: "blue", height: "100%" }}>
+      <Grid
+        container
+        spacing={3}
+        sx={{
+          backgroundColor: "pink",
+          width: "100%",
+          margin: "auto",
+          padding: "5px",
+          height: "100%",
+        }}
+      >
+        <Grid item xs={12} md={6} lg={6}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              height: "100%",
+            }}
+          >
+            <Typography variant="h3" gutterBottom>
+              Just Say hi.
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{ display: "flex", alignItems: "center", mb: 1 }}
+            >
+              I am always open to discuss your project and talk about new things
+            </Typography>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Box>
+                {" "}
+                <Typography
+                  variant="body1"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "start",
+                    mb: 1,
+                  }}
+                >
+                  <span style={{ marginRight: "8px" }}>Mail me at</span>{" "}
+                  anilc7089@gmail.com
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "start",
+                  mb: 1,
+                }}
+              >
+                <Typography variant="body1">Follow me</Typography>
+                <Box></Box>
+              </Box>
+            </Box>
+            {/* <Typography variant="body1" sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
                 <span style={{ marginRight: '8px' }}>Email:</span> john.doe@example.com
               </Typography>
               <Typography variant="body1" sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
@@ -34,42 +91,91 @@ const ContactMe = () => {
               </Typography>
               <Typography variant="body1" sx={{ display: 'flex', alignItems: 'center' }}>
                 <span style={{ marginRight: '8px' }}>Contact Number:</span> (123) 456-7890
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item  xs={12} md={6} lg={6}>
-            <Box sx={{ display: 'flex',backgroundColor:"yellow", flexDirection: 'column', justifyContent: 'center', height: '100%'}}>
-              <Typography variant="h6" gutterBottom>Get in touch</Typography>
-              {submitted ? (
-                <Typography variant="body1" align="center" sx={{ mb: 2 }}>Thank you for your message! I'll get back to you as soon as possible.</Typography>
-              ) : (
-                <form>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                      <TextField label="Your Name" fullWidth margin="normal"  variant="standard" name="name" value={formData.name} onChange={handleChange} />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField label="Your Email" fullWidth margin="normal"  variant="standard" name="email" value={formData.email} onChange={handleChange} />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField label="Your budget" fullWidth margin="normal"  variant="standard" name="budget" value={formData.email} onChange={handleChange} />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField label="Your Project Description" fullWidth margin="normal"  variant="standard" multiline rows={4} name="description" value={formData.message} onChange={handleChange} />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-                        <Button variant="contained" color="primary" disabled={submitting}>
-                          Send
-                        </Button>
-                      </Box>
-                    </Grid>
-                  </Grid>
-                </form>
-              )}
-            </Box>
-          </Grid>
+              </Typography> */}
+          </Box>
         </Grid>
+        <Grid item xs={12} md={6} lg={6} sx={{ padding: "10px" }}>
+          <Box
+            sx={{
+              display: "flex",
+              backgroundColor: "yellow",
+              flexDirection: "column",
+              justifyContent: "center",
+              height: "100%",
+            }}
+          >
+            {/* <Typography variant="h4" gutterBottom>Get in touch</Typography> */}
+            {submitted ? (
+              <Typography variant="body1" align="center" sx={{ mb: 2 }}>
+                Thank you for your message! I'll get back to you as soon as
+                possible.
+              </Typography>
+            ) : (
+              <form>
+                <Grid container spacing={2} sx={{ padding: "0 20px 5px 20px" }}>
+                  <Grid item xs={12}>
+                    <TextField
+                      label="Your Name"
+                      fullWidth
+                      margin="normal"
+                      variant="standard"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      label="Your Email"
+                      fullWidth
+                      margin="normal"
+                      variant="standard"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      label="Your budget"
+                      fullWidth
+                      margin="normal"
+                      variant="standard"
+                      name="budget"
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      label="Your Project Description"
+                      fullWidth
+                      margin="normal"
+                      variant="standard"
+                      multiline
+                      rows={4}
+                      name="description"
+                      value={formData.message}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box sx={{ display: "flex", justifyContent: "end" }}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        disabled={submitting}
+                      >
+                        Submit
+                      </Button>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </form>
+            )}
+          </Box>
+        </Grid>
+      </Grid>
       {/* <Container maxWidth="lg">
       </Container> */}
     </Box>
